08e63b9f7b4a31776364a6469960caed703caa63
Karan <Karansj4723@gmail.com>
2024-07-25 06:55:57 +0530

    modified details of database

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import datetime
 from flask import Flask, json, redirect, render_template, request, url_for
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
 
 # Configuration for MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'newsapp'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 mysql = MySQL(app)
 
